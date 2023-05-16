d22878dae35d7a09b5ab327b535d3bcb77281198
FilinGoy <den_suvorov2003@mail.ru>
2023-05-17 03:11:48 +0500

    SmallFix

--- resources/views/admin/product/show.blade.php
+++ resources/views/admin/product/show.blade.php
@@ -6,7 +6,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">Пользователь</h1>
+                    <h1 class="m-0">Продукт</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
@@ -57,7 +57,7 @@
                             <dt>Описание</dt>
                             <dd>{{ $product->description }}</dd>
                             <dt>Публикация</dt>
-                            <dd>{{ $product->published ? 'Не опубликовано' : 'Опубликовано' }}</dd>
+                            <dd>{{ $product->published ? 'Опубликовано' : 'Не опубликовано' }}</dd>
                             <dt>Превью</dt>
                             <dd class="row">
                                 <div class="position-relative col-3">

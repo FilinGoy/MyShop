46dfc9be7eea01082fca8891b351e45ae4a8e603
FilinGoy <den_suvorov2003@mail.ru>
2023-06-02 12:20:20 +0500

    Fix

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -178,6 +178,7 @@
                         data-accordion="false">
                         <li class="nav-item">
                             <a href="/" class="nav-link">
+                                <i class="nav-icon fas fa-door-open"></i>
                                 <p>
                                     В магазин
                                 </p>
